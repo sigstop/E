0fd882b65cd343df793395a914d8c4a65b90717b
Stu Bailey <sbailey@sbaileymac.local>
2012-08-24 23:53:41 -0700

    Added support for animation

--- src/elib.erl
+++ src/elib.erl
@@ -2,7 +2,18 @@
 -include("../include/e.hrl").
 -compile([export_all]).
 
-create_window(ID,Height,Width,Title ) when is_atom(ID),
+create_window(ID,Width,Height,Title ) when is_atom(ID),
 					   is_list(Title)->
-    econnect:send(eserver,{create_window,ID,float(Height),float(Width),Title}).
+    econnect:send(eserver,{create_window,ID,float(Width),float(Height),Title}).
+
+window_add_image(WindowID,ImageID,ImageWidth,ImageHeight,ImageData) when is_atom(WindowID),
+									 is_atom(ImageID),
+									 is_binary(ImageData)->
+    econnect:send(eserver,{window_add_image,WindowID,
+			   ImageID,float(ImageWidth),float(ImageHeight),ImageData}).
+
+window_update_image(WindowID,ImageID,ImageData) when is_atom(WindowID),
+						     is_atom(ImageID),
+						     is_binary(ImageData)->
+    econnect:send(eserver,{window_update_image,WindowID,ImageID,ImageData}).
 
--- www/js/e.js
+++ www/js/e.js
@@ -25,13 +25,16 @@ var emsg_handle = function(tuple) {
     case 'window_add_image':
 	windowAddImage(tuple);  
 	break;
+    case 'window_update_image':
+	windowUpdateImage(tuple);
+	break;
     default: 
 	alert("The EClient sent unknown command: " + command)
     }
 };
     
 var createWindow = function(tuple) {
-    var ID = tuple[1];
+    var ID = tuple[1].value;
     var Width = tuple[2];
     var Height = tuple[3];
     var Title = tuple[4];
@@ -49,21 +52,38 @@ var createWindow = function(tuple) {
 };
 
 var windowAddImage = function(tuple) {
-    var WindowID = tuple[1];
-    var ImageID = tuple[2];
+    var WindowID = tuple[1].value;
+    var ImageID = tuple[2].value;
+    var ImageWidth = tuple[3];
+    var ImageHeight = tuple[4];
+    var Data = tuple[5].value;
+
+    var img =  "data:image/png;base64," + window.btoa(Data);
+
+    var Image = new Ext.Component({
+	autoEl: { tag: 'img', width: ImageWidth, height: ImageHeight, src: img}
+    });
+
+    windows[WindowID].add( Image );
+    windows[WindowID].doLayout();
+    images[ImageID] = Image;
+};
+
+var windowUpdateImage = function(tuple) {
+    var WindowID = tuple[1].value;
+    var ImageID = tuple[2].value;
     var Data = tuple[3].value;
 
     var img =  "data:image/png;base64," + window.btoa(Data);
 
     var Image = new Ext.Component({
-	autoEl: { tag: 'img', height: 200, width : 200, src: img}
+	autoEl: { tag: 'img', width: images[ImageID].autoEl.width, height: images[ImageID].autoEl.height, src: img}
     });
 
-    windows[WindowID].add( Image );
+    windows[WindowID].remove(images[ImageID]);
+    windows[WindowID].add(Image);
     windows[WindowID].doLayout();
-
     images[ImageID] = Image;
-
 };
 	
 connection.onmessage = function(msg) {
